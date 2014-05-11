5468a014ea31d32fbba660af93d579a6aaf88cd0
Alex.Liu <alexliubo@gmail.com>
2014-05-11 06:36:49 -0500

    modify tires and accessories data access objects

--- OBG_System/DataAccess/AccessoriesDAO.cs
+++ OBG_System/DataAccess/AccessoriesDAO.cs
@@ -11,24 +11,79 @@ namespace DataAccess
 {
     public static class AccessoriesDAO
     {
+        private static DbHelper db = new DbHelper();
         public static DataTable GetAllAccessories()
         {
-            return new DataTable();
+            DbCommand command = db.GetSqlStringCommond(@"
+                                        SELECT [partNo]
+                                              ,[accId]
+                                              ,[image]
+                                              ,[des]
+                                              ,[pricing]
+                                              ,[categoryId]
+                                              ,[name]
+                                          FROM [Accessories]");
+            DataTable dt = db.ExecuteDataTable(command);
+            return dt;
         }
 
         public static int UpdateAccessory(Accessories acc)
         {
-            return 0;
+            DbCommand command = db.GetSqlStringCommond(@"UPDATE [Accessories]
+                                       SET [partNo] = @
+                                          ,[image] = @
+                                          ,[des] = @
+                                          ,[pricing] = @
+                                          ,[categoryId] = @
+                                          ,[name] = @
+                                     WHERE accId= @accId");
+            SqlParameter[] paras = new SqlParameter[] { 
+                new SqlParameter("@partNo", acc.PartNo),
+                new SqlParameter("@image",acc.Img),
+                new SqlParameter("@des",acc.Des),
+                new SqlParameter("@pricing",acc.Pricing),
+                new SqlParameter("@categoryId",acc.CategoryId),
+                new SqlParameter("@name",acc.Name),
+                 new SqlParameter("@accId",acc.AccId)
+            };
+            command.Parameters.AddRange(paras);
+            return db.ExecuteNonQuery(command);
         }
 
         public static int CreateNewAccessory(Accessories acc)
         {
-            return 0;
+            DbCommand command = db.GetSqlStringCommond(@"INSERT INTO [Accessories]
+                                                   ([partNo]
+                                                   ,[image]
+                                                   ,[des]
+                                                   ,[pricing]
+                                                   ,[categoryId]
+                                                   ,[name])
+                                             VALUES
+                                                   @partNo
+                                                   ,@image
+                                                   ,@des
+                                                   ,@pricing
+                                                   ,@categoryId
+                                                   ,@name");
+            SqlParameter[] paras = new SqlParameter[] { 
+                new SqlParameter("@partNo", acc.PartNo),
+                new SqlParameter("@image",acc.Img),
+                new SqlParameter("@des",acc.Des),
+                new SqlParameter("@pricing",acc.Pricing),
+                new SqlParameter("@categoryId",acc.CategoryId),
+                new SqlParameter("@name",acc.Name)
+            };
+            command.Parameters.AddRange(paras);
+            return db.ExecuteNonQuery(command);
         }
 
         public static int DeleteOneAccessory(int accId)
         {
-            return 0;
+            DbCommand command = db.GetSqlStringCommond(@"  delete from [Accessories] where accId = @accId;");
+            SqlParameter[] paras = new SqlParameter[] { new SqlParameter("@accId", accId) };
+            command.Parameters.AddRange(paras);
+            return db.ExecuteNonQuery(command);
         }
     }
 }
--- OBG_System/DataAccess/TiresDAO.cs
+++ OBG_System/DataAccess/TiresDAO.cs
@@ -11,24 +11,84 @@ namespace DataAccess
 {
     public static class TiresDAO
     {
+        private static DbHelper db = new DbHelper();
         public static DataTable GetAllTires()
         {
-            return new DataTable();
+            DbCommand command = db.GetSqlStringCommond(@"SELECT [tireId]
+                                                      ,[partNo]
+                                                      ,[size]
+                                                      ,[rimWith]
+                                                      ,[rimHeight]
+                                                      ,[pricing]
+                                                      ,[season]
+                                                      ,[categoryId]
+                                                  FROM [Tires]");
+            DataTable dt = db.ExecuteDataTable(command);
+            return dt;
         }
 
         public static int CreateNewTire(Tire tire)
         {
-            return 0;
+            DbCommand command = db.GetSqlStringCommond(@"INSERT INTO [Tires]
+                                                       ([partNo]
+                                                       ,[size]
+                                                       ,[rimWith]
+                                                       ,[rimHeight]
+                                                       ,[pricing]
+                                                       ,[season]
+                                                       ,[categoryId])
+                                                 VALUES
+                                                       (@partNo
+                                                       ,@size
+                                                       ,@rimWith
+                                                       ,@rimHeight
+                                                       ,@pricing
+                                                       ,@season
+                                                       ,@categoryId)");
+            SqlParameter[] paras = new SqlParameter[] { 
+                new SqlParameter("@partNo", tire.PartNo),
+                new SqlParameter("@size",tire.Size),
+                new SqlParameter("@rimWith",tire.Width),
+                new SqlParameter("@rimHeight",tire.Height),
+                new SqlParameter("@pricing",tire.Pricing),
+                new SqlParameter("@season",tire.Season),
+                new SqlParameter("@categoryId",tire.CategoryId)
+            };
+            command.Parameters.AddRange(paras);
+            return db.ExecuteNonQuery(command);
         }
 
         public static int DeleteTire(int tireId)
         {
-            return 0;
+            DbCommand command = db.GetSqlStringCommond(@"delete from Tires where tireId =@tireId");
+            SqlParameter[] paras = new SqlParameter[] { new SqlParameter("@tireId", tireId) };
+            command.Parameters.AddRange(paras);
+            return db.ExecuteNonQuery(command);
         }
 
         public static int UpdateTire(Tire tire)
         {
-            return 0;
+            DbCommand command = db.GetSqlStringCommond(@"UPDATE [OBG_].[dbo].[Tires]
+                                   SET [partNo] = @partNo
+                                      ,[size] = @size
+                                      ,[rimWith] = @rimWith
+                                      ,[rimHeight] = @rimHeight
+                                      ,[pricing] = @pricing
+                                      ,[season] = @season
+                                      ,[categoryId] = @categoryId
+                                 WHERE tireId = @tireId)");
+            SqlParameter[] paras = new SqlParameter[] { 
+                new SqlParameter("@partNo", tire.PartNo),
+                new SqlParameter("@size",tire.Size),
+                new SqlParameter("@rimWith",tire.Width),
+                new SqlParameter("@rimHeight",tire.Height),
+                new SqlParameter("@pricing",tire.Pricing),
+                new SqlParameter("@season",tire.Season),
+                new SqlParameter("@categoryId",tire.CategoryId),
+                new SqlParameter("@tireId",tire.TireId)
+            };
+            command.Parameters.AddRange(paras);
+            return db.ExecuteNonQuery(command);
         }
     }
 }
