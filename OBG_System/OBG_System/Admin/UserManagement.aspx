b4b85c1de108e3530d7787bf0111ebad48eb7e82
neobility <neo.wu2@gmail.com>
2014-05-11 19:12:52 -0400

    created User management Gridview

--- OBG_System/OBG_System/Admin/UserManagement.aspx
+++ OBG_System/OBG_System/Admin/UserManagement.aspx
@@ -4,5 +4,31 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
 
+    <asp:GridView ID="GridView1" runat="server" AllowPaging="True" AutoGenerateColumns="False" CellPadding="4" DataKeyNames="UserId"  ForeColor="#333333" GridLines="None" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing"
+                        OnRowUpdating="GridView1_RowUpdating" OnRowCancelingEdit="GridView1_RowCancelingEdit">
+    <AlternatingRowStyle BackColor="White" />
+    <Columns>
+        <asp:BoundField DataField="UserId" HeaderText="UserId" InsertVisible="False" ReadOnly="True" SortExpression="UserId" />
+        <asp:BoundField DataField="UserName" HeaderText="UserName" SortExpression="UserName" />
+        <asp:BoundField DataField="Status" HeaderText="Status" SortExpression="Status" />
+        <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+        <asp:BoundField DataField="CompanyName" HeaderText="CompanyName" SortExpression="CompanyName" />
+        <asp:BoundField DataField="Phone" HeaderText="Phone" SortExpression="Phone" />
+        <asp:CommandField HeaderText="Select" ShowSelectButton="True" />
+        <asp:CommandField HeaderText="Edit" ShowEditButton="True" />
+        <asp:CommandField HeaderText="Delete" ShowDeleteButton="True" />
+    </Columns>
+    <EditRowStyle BackColor="#2461BF" />
+    <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+    <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+    <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+    <RowStyle BackColor="#EFF3FB" />
+    <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+    <SortedAscendingCellStyle BackColor="#F5F7FB" />
+    <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+    <SortedDescendingCellStyle BackColor="#E9EBEF" />
+    <SortedDescendingHeaderStyle BackColor="#4870BE" />
+</asp:GridView>
+
 </asp:Content>
 
--- OBG_System/OBG_System/Admin/UserManagement.aspx.cs
+++ OBG_System/OBG_System/Admin/UserManagement.aspx.cs
@@ -4,11 +4,85 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using OBGModel;
+using BusinessLogic;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class Admin_Default : System.Web.UI.Page
 {
+    private DataSet userDataSet;
+
+    //SqlConnection sqlcon;
+    //SqlCommand sqlcom;
+    //string strCon = "Data Source=(local);Database=数据库名;Uid=帐号;Pwd=密码";
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        //DataTable usersTable = UserBLO.GetAllUsers();
+        //dataSet = new DataSet();
+        //dataSet.Tables.Add(usersTable);
+        if (!IsPostBack)
+        {
+            bind();
+        }
+    }
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        bind();
+    }
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        //string sqlstr = "delete from 表 where id='" + GridView1.DataKeys[e.RowIndex].Value.ToString() + "'";
+        //sqlcon = new SqlConnection(strCon);
+        //sqlcom = new SqlCommand(sqlstr, sqlcon);
+        //sqlcon.Open();
+        //sqlcom.ExecuteNonQuery();
+        //sqlcon.Close();
+        int userID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+        UserBLO.RemoveUserById(userID);
+        bind();
+    }
 
+    protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        //sqlcon = new SqlConnection(strCon);
+        //string sqlstr = "update 表 set 字段1='"
+        //    + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[1].Controls[0])).Text.ToString().Trim() + "',字段2='"
+        //    + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[2].Controls[0])).Text.ToString().Trim() + "',字段3='"
+        //    + ((TextBox)(GridView1.Rows[e.RowIndex].Cells[3].Controls[0])).Text.ToString().Trim() + "' where id='"
+        //    + GridView1.DataKeys[e.RowIndex].Value.ToString() + "'";
+        //sqlcom = new SqlCommand(sqlstr, sqlcon);
+        //sqlcon.Open();
+        //sqlcom.ExecuteNonQuery();
+        //sqlcon.Close();
+        //GridView1.EditIndex = -1;
+        //bind();
+    }
+
+    protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        bind();
+    }
+
+    public void bind()
+    {
+        //string sqlstr = "select * from 表";
+        //sqlcon = new SqlConnection(strCon);
+        //SqlDataAdapter myda = new SqlDataAdapter(sqlstr, sqlcon);
+        //DataSet myds = new DataSet();
+        //sqlcon.Open();
+        //myda.Fill(myds, "表");
+
+        DataTable usersTable = UserBLO.GetAllUsers();
+        userDataSet = new DataSet();
+        userDataSet.Tables.Add(usersTable);
+
+        GridView1.DataSource = userDataSet;
+        GridView1.DataKeyNames = new string[] { "userid" };//主键
+        GridView1.DataBind();
+       // sqlcon.Close();
     }
 }
