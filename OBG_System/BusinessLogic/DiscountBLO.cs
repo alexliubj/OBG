cda1a04ef98046af8d174c678969532571e861af
Alex.Liu <alexliubo@gmail.com>
2014-05-11 20:51:02 -0500

    finish region+discount DAO

--- OBG_System/BusinessLogic/DiscountBLO.cs
+++ OBG_System/BusinessLogic/DiscountBLO.cs
@@ -4,28 +4,27 @@ using System.Linq;
 using System.Text;
 using OBGModel;
 using DataAccess;
-
+using System.Data;
 namespace BusinessLogic
 {
     public static class DiscountBLO
     {
-
-        public static List<Discount> GetAllDiscount()
+        public static DataTable GetAllDiscount()
         {
             return DiscountDAO.GetAllDiscount();
         }
 
-        public static bool AddDiscount(int roleId, float rate)
+        public static int AddDiscount(int roleId, float rate)
         {
             return DiscountDAO.AddDiscount(roleId,rate);
         }
 
-        public static bool UpdateDiscount(int roleId, float rate)
+        public static int UpdateDiscount(int roleId, float rate)
         {
             return DiscountDAO.UpdateDiscount(roleId, rate);
         }
 
-        public static bool DeleteDiscount(int roleId)
+        public static int DeleteDiscount(int roleId)
         {
             return DiscountDAO.DeleteDiscount(roleId);
         }
