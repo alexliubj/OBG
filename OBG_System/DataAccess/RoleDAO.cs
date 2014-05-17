cb3e44785629e34271e303d35f91e6ded9cb9e0b
Alex.Liu <alexliubo@gmail.com>
2014-05-17 03:15:02 -0500

    Merge userrole dao and update to the latest version

--- OBG_System/DataAccess/RoleDAO.cs
+++ OBG_System/DataAccess/RoleDAO.cs
@@ -59,7 +59,7 @@ namespace DataAccess
 
         public static int DeleteRoleByRoleId(int roleId)
         {
-            DbCommand command = db.GetSqlStringCommond(@"delete from discount where roleId = @roleId");
+            DbCommand command = db.GetSqlStringCommond(@"delete from role where roleId = @roleId");
             SqlParameter[] paras = new SqlParameter[] { new SqlParameter("@roleId", roleId) };
             command.Parameters.AddRange(paras);
             return db.ExecuteNonQuery(command);
@@ -71,7 +71,7 @@ namespace DataAccess
 
         #region User and Role Managements
 
-        public static int AddUserToRole(int userid, int roleId,string des)
+        public static int AddUserToRole(int userid, int roleId, string des)
         {
             DbCommand command = db.GetSqlStringCommond(@"
                             INSERT INTO [UserRole]
@@ -113,23 +113,18 @@ namespace DataAccess
             return retRole;
         }
 
-
-        // haven't finish yet
         public static int UpdateUserRole(int userId, int roleId, string des)
-        { 
-            
+        {
             DbCommand command = db.GetSqlStringCommond(@"
                             UPDATE  [UserRole]
-                               SET [RoleId] = @userId
-                                  ,[UserId] = @roleId
+                               SET [RoleId] = @roleId
                                   ,[Des] = @des
-                             WHERE <Search Conditions,,>");
+                             WHERE userid=@userid");
             SqlParameter[] paras = new SqlParameter[] { 
                 new SqlParameter("@userid", userId) , 
             new SqlParameter("@roleId",roleId)};
             command.Parameters.AddRange(paras);
             return db.ExecuteNonQuery(command);
-
         }
 
         public static int DeleteUserFromRole(int userid, int roleId)
@@ -158,7 +153,6 @@ namespace DataAccess
             DataTable dt = db.ExecuteDataTable(command);
             return dt;
         }
-
         #endregion User and Role Managements
     }
 }
