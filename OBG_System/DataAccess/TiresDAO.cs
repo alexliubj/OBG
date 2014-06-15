12330f531bd78c71390269d95cef685b4edc4004
Alex.Liu <alexliubo@gmail.com>
2014-06-15 03:40:57 -0500

    Revert "modify getTires and getAcc by userId"
    This reverts commit ee2d4d395c17a5c17dde599ecc8338a2baca3f27.

--- OBG_System/DataAccess/TiresDAO.cs
+++ OBG_System/DataAccess/TiresDAO.cs
@@ -27,30 +27,12 @@ namespace DataAccess
             return dt;
         }
 
-        public static DataTable GetAllTires(int userId)
-        {
-            DbCommand command = db.GetSqlStringCommond(@"SELECT [tireId]
-                                                      ,[partNo]
-                                                      ,[image]
-                                                      ,[size]
-                                                      ,[pricing]
-                                                      ,[season]
-                                                      ,[brand]
-                                                      ,[des]
-                                                  ,d.tiresRate,t.pricing*d.tiresRate finalprice
-                                                  FROM dbo.[Tires] t ,[discount] d where d.userid=@userid");
-            SqlParameter[] paras = new SqlParameter[] { new SqlParameter("@userid", userId)};
-            command.Parameters.AddRange(paras);
-            DataTable dt = db.ExecuteDataTable(command);
-            return dt;
-        }
-
         public static int CreateNewTire(Tire tire)
         {
             DbCommand command = db.GetSqlStringCommond(@"INSERT INTO [Tires]
                                                        ([partNo]
                                                        ,[image]
-                                                       ,[size]
+                                                       ,[size]                                                      
                                                        ,[pricing]
                                                        ,[season],[brand],[des])
                                                  VALUES
