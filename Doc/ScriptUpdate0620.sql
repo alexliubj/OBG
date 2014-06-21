50bc16d9cbe3e7622de7ab6c8d09ae7c09e8dc85
Alex.Liu <alexliubo@gmail.com>
2014-06-20 21:02:15 -0500

    add new script

---
+++ Doc/ScriptUpdate0620.sql
@@ -0,0 +1,3 @@
+alter table accessories add special float not null default 1.0;
+alter table tires add special float not null default 1.0;
+alter table wheels add special float not null default 1.0;
